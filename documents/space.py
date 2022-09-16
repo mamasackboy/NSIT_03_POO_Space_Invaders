cc3c3ce5bac645a4a6a49ccf623f9247d84436ed
mamasackboy <99284014+mamasackboy@users.noreply.github.com>
2022-09-16 12:27:32 +0200

    Add files via upload

---
+++ documents/space.py
@@ -0,0 +1,7 @@
+import pygame  # necessaire pour charger les images et les sons
+
+
+class Joueur() : # classe pour créer le vaisseau du joueur
+    def __init__(self,sens) :
+        
+        pass
