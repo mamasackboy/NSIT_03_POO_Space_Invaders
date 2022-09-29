eb91240145b0f105f2e03804edb58d3e76ad3cd5
mamasackboy <99284014+mamasackboy@users.noreply.github.com>
2022-09-29 22:11:32 +0200

    Add files via upload

--- documents/space.py
+++ documents/space.py
@@ -1,14 +1,15 @@
 import pygame  # necessaire pour charger les images et les sons
 import random
+import math
 
 class joueur() : # classe pour créer le vaisseau du joueur
     def __init__(self) :
         self.position = 475
         self.image = pygame.image.load("vaisseau.png")
         self.sens = "O"
-        self.vitesse = 2
+        self.vitesse = 5
         self.score=0
-
+        self.lives=3
     def deplacer(self) :
         if (self.sens == "droite") and (self.position < 930):
             self.position = self.position + self.vitesse
@@ -16,9 +17,8 @@ class joueur() : # classe pour créer le vaisseau du joueur
            self.position = self.position - self.vitesse
     def tirer(self):
         self.sens = "O"
-    def marquer():
-        if Balle.toucher(Ennemi):
-            self.score+=1
+    def marquer(self):
+        self.score+=1
         
 class Balle() :
     def __init__(self, tireur):
@@ -27,39 +27,64 @@ class Balle() :
         self.hauteur = 492
         self.image = pygame.image.load("balle.png")
         self.etat = "chargee"
+        self.vitesse=8
     
     def bouger(self):
-        if self.etat == "attente":
+        if self.etat == "chargee":
             self.depart = self.tireur.position + 16
             self.hauteur = 492
         elif self.etat == "tiree" :
-            self.hauteur = self.hauteur - 5
-        
+            self.hauteur = self.hauteur - self.vitesse
         if self.hauteur < 0:
             self.etat = "chargee"
-    def toucher(Ennemi,joueur):
-        if self.hauteur==Ennemi.hauteur:
-            self.hauteur=492
-            self.etat="chargee"
+
+    def toucher(self,vaisseau,player):
+        if (math.fabs(self.depart - vaisseau.depart) < 40) and (math.fabs(self.hauteur - vaisseau.hauteur) < 40):
+            return True
+        else:
+            if (math.fabs(self.depart - vaisseau.depart) < 40) and (math.fabs(self.hauteur - vaisseau.hauteur) < 40):
+                vaisseau.disparaitre()
+                player.score -= 1
+
         
 class Ennemi():
-    NbEnnemis = 6
+    NbEnnemis = 9
     
     def __init__(self):
         self.depart = random.randint(1,700)
         self.hauteur = 10
-        self.type = random.randint(1,2)
+        self.type = random.randint(1,4)
         if  (self.type == 1):
             self.image = pygame.image.load("invader1.png")
-            self.vitesse = 1
+            self.vitesse = 0.5
         elif (self.type ==2):
             self.image = pygame.image.load("invader2.png")
+            self.vitesse = 1
+        elif (self.type ==3):
+            self.image = pygame.image.load("invader3.png")
+            self.vitesse = 1.5
+        elif (self.type ==4):
+            self.image = pygame.image.load("invader4.png")
             self.vitesse = 2
             
     def avancer(self):
         self.hauteur = self.hauteur + self.vitesse
     def disparaitre(self):
-        if Balle.hauteur==self.hauteur:
-            self.depart = random.randint(60,663)
+        self.depart = random.randint(1, 700)
+        self.hauteur = 10
+        self.type = random.randint(1, 4)
+        if self.type == 1:
+            self.image = pygame.image.load('invader1.png')
+            self.vitesse = 0.75
+        elif self.type == 2:
+            self.image = pygame.image.load('invader2.png')
+            self.vitesse = 2
+        elif self.type == 3:
+            self.image = pygame.image.load('invader3.png')
+            self.vitesse = 1
+        elif self.type == 3:
+            self.image = pygame.image.load('invader4.png')
+            self.vitesse = 1.5
+
     
 
--- documents/space_invaders_5.py
+++ documents/space_invaders_5.py
@@ -1,4 +1,5 @@
 import pygame # importation de la librairie pygame
+from pygame import mixer
 import space
 import sys # pour fermer correctement l'application
 import random
@@ -7,10 +8,13 @@ pygame.init()
 
 # création d'une fenêtre de 800 par 600
 screen = pygame.display.set_mode((1020,600))
-pygame.display.set_caption("Space Invaders")
+pygame.display.set_caption("Bienvenue dans le Space Invaders de Mamadou")
 clock=pygame.time.Clock()
+mixer.music.load('background.wav')
+mixer.music.play(-1)
 # chargement de l'image de fond
 fond = pygame.image.load('background.png')
+font=pygame.font.Font('freesansbold.ttf',32)
 
 # creation du joueur
 player = space.joueur()
@@ -29,8 +33,11 @@ running = True # variable pour laisser la fenêtre ouverte
 while running : # boucle infinie pour laisser la fenêtre ouverte
     # dessin du fond
     screen.blit(fond,(0,0))
+    score = font.render("Points: " + str(player.score),True, (255, 255, 0))
+    screen.blit(score, (800, 30))
+
     clock.tick(60)
-    ### Gestion des événements  ###
+    #affichage du score    ### Gestion des événements  ###
     for event in pygame.event.get(): # parcours de tous les event pygame dans cette fenêtre
         if event.type == pygame.QUIT : # si l'événement est le clic sur la fermeture de la fenêtre
             running = False # running est sur False
@@ -49,7 +56,7 @@ while running : # boucle infinie pour laisser la fenêtre ouverte
     ### Actualisation de la scene ###
     # Gestions des collisions
     for ennemi in listeEnnemis:
-        if tir.toucher(ennemi):
+        if tir.toucher(ennemi,player):
             ennemi.disparaitre()
             player.marquer()
     print(f"Score = {player.score} points")
@@ -64,5 +71,8 @@ while running : # boucle infinie pour laisser la fenêtre ouverte
     for ennemi in listeEnnemis:
         ennemi.avancer()
         screen.blit(ennemi.image,[ennemi.depart, ennemi.hauteur]) # appel de la fonction qui dessine le vaisseau du joueur
-        
+        if ennemi.hauteur > 600:
+            ennemi.disparaitre()
+            ennemi.avancer()
+
     pygame.display.update() # pour ajouter tout changement à l'écran
